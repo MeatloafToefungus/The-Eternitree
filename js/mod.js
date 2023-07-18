8cac5ea77ea19c8e8390b9452c488655fe28f1a0
MeatloafToefungus <139905433+MeatloafToefungus@users.noreply.github.com>
2023-07-18 12:57:53 -0400

    ok
    ok

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Eternitree",
+	id: "eternitree",
+	author: "The Big Man, Llama",
+	pointsName: "kromer",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
@@ -22,7 +22,7 @@ let changelog = `<h1>Changelog:</h1><br>
 		- Added things.<br>
 		- Added stuff.`
 
-let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
+let winText = `Ok.`
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
 // (The ones here are examples, all official functions are already taken care of)
@@ -56,7 +56,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
+	return player.points.gte(new Decimal("1.79e308"))
 }
 
 
